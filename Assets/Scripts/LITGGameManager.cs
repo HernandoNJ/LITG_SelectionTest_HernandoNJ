7696e23493ee7c86c7721c8926bec16eb8d9e176
Hernando NJ <hernandonj.unity@gmail.com>
2022-08-18 23:30:56 -0500

    Progress in Scene1 and Scene2

---
+++ Assets/Scripts/LITGGameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LITGGameManager : MonoBehaviour
+{
+    
+    
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
